ee8dd34e2da54ea159095ae3e07d6c2644463b4e
ddrosario <drewdelrosario@gmail.com>
2022-04-29 10:16:49 -0700

    Add form-data required for Mailgun v3

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-
+const formData = require('form-data');
 const Mailgun = require('mailgun.js');
 
 const SimpleMailgunAdapter = mailgunOptions => {
   if (!mailgunOptions || !mailgunOptions.apiKey || !mailgunOptions.domain || !mailgunOptions.fromAddress) {
     throw 'SimpleMailgunAdapter requires an API Key, domain, and fromAddress.';
   }
-  const mailgunClient = Mailgun.client({
+  const mailgun = new Mailgun(formData);
+  const mailgunClient = mailgun.client({
     username: 'api',
     key: mailgunOptions.apiKey
   });
